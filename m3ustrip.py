6fe66dbf23391ea4d476ce35f3e33f8db9445573
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2023-03-02 01:17:41 -0500

    Add files via upload
    Added comments.

--- m3ustrip.py
+++ m3ustrip.py
@@ -6,10 +6,16 @@ playlist_files = [f for f in os.listdir('.') if f.endswith('.m3u') or f.endswith
 # loop through each playlist file and modify the lines to remove the folder data
 for filepath in playlist_files:
     with open(filepath, 'r+') as f:
+# read the lines from the playlist file
         lines = f.readlines()
+# move the file pointer back to the beginning of the file
         f.seek(0)
+# truncate the file to remove any existing content
         f.truncate()
+
+# loop through each line in the file and modify it
         for line in lines:
             if not line.startswith('#'):
                 line = os.path.basename(line)
+# write the modified line back to the playlist file
             f.write(line)
