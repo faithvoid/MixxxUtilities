79baf4c709505a918be4b35cf6ff66220cbd0861
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2023-03-02 01:14:46 -0500

    Add files via upload

--- m3ustrip.py
+++ m3ustrip.py
@@ -1,28 +1,15 @@
 import os
 
-# specify the directory where the .m3u files are located
-directory = os.getcwd()
+# get the list of .m3u and .m3u8 files in the current directory
+playlist_files = [f for f in os.listdir('.') if f.endswith('.m3u') or f.endswith('.m3u8')]
 
-# loop through each .m3u file in the directory
-for filename in os.listdir(directory):
-    if filename.endswith(".m3u"):
-        filepath = os.path.join(directory, filename)
-        
-        # read the contents of the .m3u file
-        with open(filepath, "r") as f:
-            lines = f.readlines()
-        
-        # modify the lines to remove the folder data
-        modified_lines = []
+# loop through each playlist file and modify the lines to remove the folder data
+for filepath in playlist_files:
+    with open(filepath, 'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        f.truncate()
         for line in lines:
-            if line.startswith("#") or line.strip() == "":
-                # ignore comments and empty lines
-                modified_lines.append(line)
-            else:
-                # remove the folder data
-                filename = os.path.basename(line.strip())
-                modified_lines.append(filename + "\n")
-        
-        # write the modified contents back to the .m3u file
-        with open(filepath, "w") as f:
-            f.writelines(modified_lines)
+            if not line.startswith('#'):
+                line = os.path.basename(line)
+            f.write(line)
--- m3ustrip.sh
+++ m3ustrip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# loop through each .m3u file in the current directory
-for filepath in *.m3u; do
+# loop through each .m3u and .m3u8 file in the current directory
+for filepath in *.m3u *.m3u8; do
     # modify the lines to remove the folder data
     sed -i '/^#/!s#.*/##' "$filepath"
 done
